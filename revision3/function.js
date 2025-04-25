c325afb4ab04d12ce495ba40deeba22996ae1898
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-04-25 09:52:56 +0530

    function

---
+++ revision3/function.js
@@ -0,0 +1,26 @@
+// normal function // function keyword declare
+// arrow function   // arrow comes in es6 => shortcut hand
+// variable function // those are declare with variable
+// anonymous  function // have no name
+
+print();
+
+function print() {
+  console.log("hello world");
+  return 
+ 
+}
+
+print();
+
+
+let show = () => {
+  console.log("arrow function");
+};
+show()
+
+let show2 = function () {
+  console.log("normal function with variable  function");
+};
+
+show2()
