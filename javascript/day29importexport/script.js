752cd411673510570b519fc4534f1317cc2af56f
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-04-25 16:38:03 +0530

    import export

---
+++ javascript/day29importexport/script.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const image = "https://m.media-amazon.com/images/I/51BNcD2PTfL._SY300_SX300_QL70_FMwebp_.jpg"
+
+export { sum, sub,image };
