874ead841f5dfab985f85aae6b2bb90788df01f5
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-07-17 16:38:14 +0530

    crudwithredux

--- react/day14reduxtodo/react-app/src/components/Todolist.jsx
+++ react/day14reduxtodo/react-app/src/components/Todolist.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTodo } from "../redux/todolist/todoslice";
+import { addTodo, deleteTodo, updateTodo } from "../redux/todolist/todoslice";
 
 const Todolist = () => {
   const [inputText, setInputText] = useState("");
   const todolist = useSelector((state) => state.todolistStore.todo);
+  const [boolean, setBoolean] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
   const dispatch = useDispatch();
 
+  function handleUpdate() {
+    dispatch(updateTodo({ editIndex, inputText }));
+    setBoolean(false);
+    setInputText("");
+  }
+  function handleEdit(index) {
+    setBoolean(true);
+    setInputText(todolist[index]);
+    setEditIndex(index);
+  }
   function handleAdd() {
     dispatch(addTodo(inputText));
     alert("task added sucessfully");
@@ -21,12 +33,34 @@ const Todolist = () => {
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
       />
-      <button onClick={handleAdd}>add</button>
+      {boolean ? (
+        <button onClick={handleUpdate}>update</button>
+      ) : (
+        <button onClick={handleAdd}>add</button>
+      )}
 
       <div>
         {todolist &&
-          todolist.map((item) => {
-            return <p>{item}</p>;
+          todolist.map((item, index) => {
+            return (
+              <div key={index}>
+                <p>{item}</p>
+                <button
+                  onClick={() => {
+                    handleEdit(index);
+                  }}
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => {
+                    dispatch(deleteTodo(index));
+                  }}
+                >
+                  delete
+                </button>
+              </div>
+            );
           })}
       </div>
     </div>
--- react/day14reduxtodo/react-app/src/redux/todolist/todoslice.js
+++ react/day14reduxtodo/react-app/src/redux/todolist/todoslice.js
@@ -12,10 +12,21 @@ export const todoSlice = createSlice({
       state.todo = [...state.todo, action.payload];
       localStorage.setItem("todoDatas", JSON.stringify(state.todo));
     },
+    updateTodo: (state, action) => {
+      state.todo = state.todo.map((item, i) =>
+        i == action.payload.editIndex ? action.payload.inputText : item
+      );
+      localStorage.setItem("todoDatas", JSON.stringify(state.todo));
+    },
+    deleteTodo: (state, action) => {
+      state.todo = state.todo.filter((item, i) => i != action.payload);
+
+      localStorage.setItem("todoDatas", JSON.stringify(state.todo));
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, updateTodo, deleteTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
