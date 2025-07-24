4d1992aa9b5b21257f28b30a0f38ea6ef3b33120
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-07-24 16:42:41 +0530

    firbase

---
+++ react/day15firebase/react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { signInWithPopup } from "firebase/auth";
+import React from "react";
+import { auth, provider } from "./config/firbase";
+
+const App = () => {
+  async function handleLogin() {
+    await signInWithPopup(auth, provider);
+  }
+  return (
+    <div>
+      <button onClick={handleLogin}>Google login</button>
+    </div>
+  );
+};
+
+export default App;
