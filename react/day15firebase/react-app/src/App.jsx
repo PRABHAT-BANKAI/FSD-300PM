0a8eda15720b2c7034b06d6a604d65cf0763ef77
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-07-28 20:27:40 +0530

    firebase

--- react/day15firebase/react-app/src/App.jsx
+++ react/day15firebase/react-app/src/App.jsx
@@ -1,14 +1,24 @@
 import { signInWithPopup } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import { auth, provider } from "./config/firbase";
+import Home from "./components/Home";
 
 const App = () => {
+  const [data, setData] = useState("");
   async function handleLogin() {
-    await signInWithPopup(auth, provider);
+    let response = await signInWithPopup(auth, provider);
+    // console.log(response.user.userName);
+    setData(response);
   }
+
+  console.log(data);
   return (
     <div>
-      <button onClick={handleLogin}>Google login</button>
+      {data ? (
+        <Home userName={data.user.displayName}  setData={setData}/>
+      ) : (
+        <button onClick={handleLogin}>Google login</button>
+      )}
     </div>
   );
 };
