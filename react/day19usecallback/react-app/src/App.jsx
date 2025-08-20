05ec93343f8d921cdb08f6f72a2e1106a441eaca
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-08-20 10:38:35 +0530

    usecallback info

--- react/day19usecallback/react-app/src/App.jsx
+++ react/day19usecallback/react-app/src/App.jsx
@@ -8,6 +8,16 @@ const App = () => {
     console.log(count);
   }
 
+
+  // const countInc = useCallback(() => {
+//   console.log("count2 call");
+//   setCount2(prev => prev + 1);
+// }, []);
+// Clicking "Increment Count 1" will not re-render Child.
+
+// Clicking "Increment Count 2" will increment count2 and cause only App to re-render (due to state change),
+//  but countInc remains the same, so Child doesn't re-render unless truly needed.
+
   return (
     <div>
       <h1>Count:{count}</h1>
