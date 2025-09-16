286b7ada6837d232919050dfdf0f99d524a64932
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-09-16 16:44:25 +0530

    crud with database

--- node/day7dbconnect/config/db.js
+++ node/day7dbconnect/config/db.js
@@ -1,7 +1,7 @@
 const { default: mongoose } = require("mongoose");
 
 const dbConnect = async () => {
-  await mongoose.connect("mongodb://127.0.0.1/dhruvPatners"); // collection of database
+  await mongoose.connect("mongodb://127.0.0.1/krishPatners"); // collection of database
   console.log("database is connected");
 };
 
--- node/day7dbconnect/index.js
+++ node/day7dbconnect/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { dbConnect } = require("./config/db");
+const { UserModel } = require("./models/userModels");
 const app = express();
 
 const PORT = 8080;
@@ -7,14 +8,37 @@ const PORT = 8080;
 app.use(express.urlencoded({ extended: true })); // req.body encoded
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.get("/", async (req, res) => {
+  let response = await UserModel.find({});
+
+  return res.json({
+    message: response,
+  });
 });
 
-app.post("/add", (req, res) => {
-  console.log(req.body);
+app.post("/add", async (req, res) => {
+  try {
+    console.log(req.body);
 
-  return res.send("added successfully");
+    await UserModel.create(req.body);
+    console.log("added");
+
+    return res.send("added successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  console.log(req.params.id);
+  await UserModel.findByIdAndDelete(req.params.id);
+  res.send("deleted succesfully");
+});
+
+app.put("/update/:id", async (req, res) => {
+  await UserModel.findByIdAndUpdate(req.params.id, req.body);
+
+  return res.send("updated succesfully");
 });
 
 app.listen(PORT, (err) => {
