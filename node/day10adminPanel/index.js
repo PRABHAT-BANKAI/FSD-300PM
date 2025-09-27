a654066b19891d55b37ac758525889df561491de
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-09-27 16:45:41 +0530

    admin panel

---
+++ node/day10adminPanel/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const PORT = 8080;
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use("/assets", express.static(path.join(__dirname, "/assets")));
+
+app.get("/", (req, res) => {
+  return res.render("dashboard");
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("server is not running");
+    return;
+  }
+  console.log("server is running ", PORT);
+});
