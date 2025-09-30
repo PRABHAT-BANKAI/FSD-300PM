5cb1b2ac0d9846c7a577adc5e922edc9becb79ad
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-09-30 16:43:20 +0530

    views user

--- node/day10adminPanel/routes/dashboard.js
+++ node/day10adminPanel/routes/dashboard.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const UserModel = require("../models/userModels");
 
 const dashboardRouter = express.Router();
 
@@ -10,13 +11,47 @@ dashboardRouter.get("/dashboard", (req, res) => {
   return res.render("dashboard");
 });
 
-dashboardRouter.get("/signUp",(req,res)=>{
-  return res.render("signUp")
-})
+dashboardRouter.get("/signUp", (req, res) => {
+  return res.render("signUp");
+});
 
+dashboardRouter.post("/add_user", async (req, res) => {
+  console.log(req.body);
+  try {
+    await UserModel.create(req.body);
+    console.log("added successfully");
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/signup");
+  }
+});
 
-dashboardRouter.post("/add_user",(req,res)=>{
-  console.log(req.body)
-})
+dashboardRouter.post("/login", async (req, res) => {
+  const { userName, password } = req.body;
 
+  try {
+    const userData = await UserModel.findOne({ userName });
+
+    if (userData.password === password) {
+      res.redirect("/dashboard");
+    } else {
+      console.log("password is not match");
+      res.redirect("/");
+    }
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+dashboardRouter.get("/usersTable", async (req, res) => {
+  try {
+    let store = await UserModel.find({});
+    res.render("userTables",{store});
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
 module.exports = { dashboardRouter };
