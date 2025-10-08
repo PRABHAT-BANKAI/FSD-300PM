0f079d0464de7f88dd1c18cb066218717d5138c5
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-10-08 18:39:06 +0530

    crud api

--- node/day17api/models/userModel.js
+++ node/day17api/models/userModel.js
@@ -1,19 +1,23 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    require: true,
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, //To prevent duplicate user registrations:
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    require: true,
-  },
-  password: {
-    type: String,
-    require: true,
-  },
-});
+  { timestamps: true }
+);
 
 const UserModel = mongoose.model("User", userSchema);
 
