5dea330820dba0d9ca1380ee21fd52ac47f49302
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-10-10 17:00:26 +0530

    bcrypt

--- node/day17api/routes/userRoutes.js
+++ node/day17api/routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { UserModel } = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const userRouter = express.Router();
 
 userRouter.post("/", async (req, res) => {
+  let {userName,email,password}=req.body
+    password = await bcrypt.hash(password, 10);
   try {
-    await UserModel.create(req.body);
+    await UserModel.create({userName,password,email});
     return res.status(201).json({
       message: "user registration succesfully",
     });
@@ -32,10 +35,25 @@ userRouter.get("/", async (req, res) => {
 userRouter.delete("/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    console.log(id,"delete")
+    console.log(id, "delete");
     await UserModel.findByIdAndDelete(id);
     return res.status(200).json({
       message: "user delted successfully",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error,
+    });
+  }
+});
+
+userRouter.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await UserModel.findByIdAndUpdate(id, req.body);
+    return res.status(200).json({
+      message: "user updated successfully",
     });
   } catch (error) {
     return res.status(400).json({
